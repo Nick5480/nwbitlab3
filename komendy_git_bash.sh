b04e90b533183b58df6b2852a501572cc603b282
Nick <xgfgjfgykgyu@gmail.com>
2024-04-17 21:21:55 +0200

    opis

---
+++ komendy_git_bash.sh
@@ -0,0 +1,21 @@
+git commit
+git checkout <nazwa gałęzi>
+git switch <nazwa gałęzi>
+git checkout -b <nazwa-gałęzi>
+git merge
+git branch <nazwa gałęzi>
+git rebase <nazwa gałęzi>
+git log
+git reset
+git revert
+git cherry-pick <Commit1> <Commit2> <...>
+git tag
+git describe
+git bisect
+git branch -f <nazwa gałęzi>
+git clone
+git fetch
+git pull
+git fakeTeamwork
+git push
+git branch -u
